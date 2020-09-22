6b90bf176d3534e131e65effbc02e835757dcfa4
kdh-r <kdh.r1008@gmail.com>
2020-09-22 11:57:02 +0900

    デバイス導入〜テストまで

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+  has_many :tasks
+
+  validates :name, :email, :password, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}\z/i}
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
+  devise_for :users
   root to: "tasks#index"
   resources :tasks
 end
+
