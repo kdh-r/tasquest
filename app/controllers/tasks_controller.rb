e0782d1878ab683f097bba02076fbacf6d32ab4a
kdh-r <kdh.r1008@gmail.com>
2020-09-24 21:47:00 +0900

    タスク編集画面

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,6 +18,11 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path
+  end
 
 end
 
