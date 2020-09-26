c5ca3df39bbfab9888c437909ceac8f7b93ba3e3
kdh-r <kdh.r1008@gmail.com>
2020-09-26 19:50:34 +0900

    ユーザー別で一覧変える

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:edit, :destroy]
 
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
 
   def new
