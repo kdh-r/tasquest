fb14c4deb546576a4de269bbf1ee8483c06b1ecd
kdh-r <kdh.r1008@gmail.com>
2020-09-30 02:56:17 +0900

    activehash実装

--- db/migrate/20200922055719_create_tasks.rb
+++ db/migrate/20200922055719_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.string :content, null: false
-      t.integer :point, null: false, default: 0
+      t.integer :point_id, null: false, default: 1
       t.references :user, foreign_key: true
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2020_09_29_125950) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content", null: false
-    t.integer "point", default: 0, null: false
+    t.integer "point_id", default: 1, null: false
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
