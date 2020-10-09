25ec91ada38af9949ebe852752dab6b83755078c
kdh-r <kdh.r1008@gmail.com>
2020-10-09 15:52:29 +0900

    migrationファイル修正

--- db/migrate/20200922055719_create_tasks.rb
+++ db/migrate/20200922055719_create_tasks.rb
@@ -4,7 +4,6 @@ class CreateTasks < ActiveRecord::Migration[6.0]
       t.string :content, null: false
       t.integer :point_id
       t.references :user, foreign_key: true
-      t.boolean :finished_at, default: false 
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2020_09_29_125950) do
     t.string "content", null: false
     t.integer "point_id"
     t.bigint "user_id"
-    t.boolean "finished_at", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
