7b7a012f0bf6b3b98114bccad951612627e1088e
kdh-r <kdh.r1008@gmail.com>
2020-10-09 12:45:34 +0900

    お試し修正3

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "tasks#index"
   resources :tasks
-  post 'users/:id', to: 'users#level'
-#   get 'tasks/:id', to: 'tasks#finish'
 end
