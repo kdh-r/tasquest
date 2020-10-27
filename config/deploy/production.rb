da4ff006ae66a05883688c45a08fee1ae30beaaa
kdh-r <kdh.r1008@gmail.com>
2020-10-27 13:59:57 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '52.68.22.206', user: 'ec2-user', roles: %w{app db web}
+server '54.199.93.116', user: 'ec2-user', roles: %w{app db web}
